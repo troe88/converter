1fdda4bf72ae04b7e1c0163358ad41b0accb10b6
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-09-20 12:29:38 +0300

    add java docs

--- src/main/java/com/epam/bigdata/factories/IFactory.java
+++ src/main/java/com/epam/bigdata/factories/IFactory.java
@@ -7,11 +7,26 @@ import java.util.List;
 import java.util.Map;
 
 public interface IFactory {
+
+    /**
+     * @param fieldMap Collection that represent the fields with name and data
+     *                 for the instance of object that should be generated.
+     *                 The main disadvantage of this approach is that we have to
+     *                 store field names somewhere or using reflection.
+     * @return Generated object, depends on the realisation.
+     */
     default Object generate(Map<String, String> fieldMap) {
         return generate(new ArrayList<>(fieldMap.values()));
     }
 
     Schema getSchema();
 
+    /**
+     *
+     * @param fields Collection that represent the fields with data
+     *               for the instance of object that should be generated.
+     *
+     * @return Generated object, depends on the realisation.
+     */
     Object generate(List<String> fields);
 }
