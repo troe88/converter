e8e3e5ad541ea731db0e1acff8e2ac582104b547
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-09-20 13:44:23 +0300

    fix data file names

--- src/main/java/com/epam/bigdata/factories/Destinations2Factory.java
+++ src/main/java/com/epam/bigdata/factories/Destinations2Factory.java
@@ -1,6 +1,6 @@
 package com.epam.bigdata.factories;
 
-import com.epam.bigdata.entities.Destinations2;
+import com.epam.bigdata.entities.Destinations;
 import lombok.AllArgsConstructor;
 import org.apache.avro.Schema;
 
@@ -9,7 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Destinations2Factory implements IFactory {
 
-    Destinations2 d2;
+    Destinations d2;
 
     @Override
     public Schema getSchema() {
@@ -18,7 +18,7 @@ public class Destinations2Factory implements IFactory {
 
     @Override
     public Object generate(List<String> fields) {
-        Destinations2 d = new Destinations2();
+        Destinations d = new Destinations();
         d.put(0, Long.parseLong(fields.get(0)));
         for (int i = 1; i < fields.size(); i++) {
             d.put(i, Float.parseFloat(fields.get(i)));
--- src/main/java/com/epam/bigdata/factories/TestFactory.java
+++ src/main/java/com/epam/bigdata/factories/TestFactory.java
@@ -1,24 +1,24 @@
 package com.epam.bigdata.factories;
 
-import com.epam.bigdata.entities.Test2;
+import com.epam.bigdata.entities.Test;
 import lombok.AllArgsConstructor;
 import org.apache.avro.Schema;
 
 import java.util.List;
 
 @AllArgsConstructor
-public class Test2Factory implements IFactory {
+public class TestFactory implements IFactory {
 
-    Test2 test2;
+    Test test;
 
     @Override
     public Schema getSchema() {
-        return test2.getSchema();
+        return test.getSchema();
     }
 
     @Override
     public Object generate(List<String> fields) {
-        Test2 train = new Test2();
+        Test train = new Test();
         for (int i = 0; i < fields.size(); i++) {
             train.put(i, fields.get(i));
         }
